0bc0f54a9e225109d5eb661a6de434029f70b5ef
Mr-angelo <angelcarrillo107@gmail.com>
2021-01-15 08:53:10 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers.js');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express hbs engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res) {
+
+    //res.send(salida)
+    res.render('home', {
+        nombre: 'Angelo',
+        //anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', function(req, res) {
+
+    //res.send(salida)
+    res.render('about', {
+        //anio: new Date().getFullYear()
+    });
+});
+
+app.listen(port);
